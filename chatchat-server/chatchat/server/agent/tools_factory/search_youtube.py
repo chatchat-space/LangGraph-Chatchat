675a5744d0771bff59f9294f4da86b645b40b993
yuehuazhang <yuehuazhang@tencent.com>
2025-01-19 12:20:36 +0800

    feature:api

--- chatchat-server/chatchat/server/agent/tools_factory/search_youtube.py
+++ chatchat-server/chatchat/server/agent/tools_factory/search_youtube.py
@@ -9,7 +9,4 @@ def search_youtube(query: str = Field(description="Query for Videos search")):
     from langchain_community.tools import YouTubeSearchTool
 
     tool = YouTubeSearchTool()
-    # return BaseToolOutput(tool.run(tool_input=query))
-    res = tool.run(tool_input=query)
-    print(res)
-    return BaseToolOutput(res)
+    return BaseToolOutput(tool.run(tool_input=query))
